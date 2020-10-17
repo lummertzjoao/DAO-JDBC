e8a1bb52aea057c3c8585c4227b5be6047886245
joaolummertz1@gmail.com <joaolummertz1@gmail.com>
2020-10-17 06:53:22 -0300

    Started implementation of SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+package model.dao;
+
+import model.dao.implementations.SellerDaoJDBC;
+
+public class DaoFactory {
+
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+	
+}
