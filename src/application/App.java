1aa3a7ae3208a3479f09fff1428fd0c5e4812da5
joaolummertz1@gmail.com <joaolummertz1@gmail.com>
2020-10-17 08:55:01 -0300

    Refactoring findById method

--- src/application/App.java
+++ src/application/App.java
@@ -1,9 +1,19 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 public class App {
 
 	public static void main(String[] args) {
 
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("Test 1: seller findById");
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
+		
 	}
 
 }
